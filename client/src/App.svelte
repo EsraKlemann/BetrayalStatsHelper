64298618acd6b0ce3d1d5c7483bbb4ff1f1c32af
EsraKlemann <98453430+EsraKlemann@users.noreply.github.com>
2022-03-16 18:45:01 +0100

    Characternames in dropdown

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,10 +1,38 @@
 <script>
 	export let name;
+
+    let selected;
+
+	let characters = [
+		{ id: 1, text: `Father Rhinehardt` },
+		{ id: 2, text: `Professor Longfellow` },
+		{ id: 3, text: `Peter Akimoto` },
+        { id: 4, text: `Brandon Jaspers` },
+        { id: 5, text: `JennyLeClerc` },
+        { id: 6, text: `Heather Granville` },
+        { id: 7, text: `Vivian Lopez` },
+        { id: 8, text: `Madame Zostra` },
+        { id: 9, text: `Darrin "Flash" Williams` },
+        { id: 10, text: `Ox Bellows` },
+        { id: 11, text: `Zoe Ingstrom` },
+        { id: 12, text: `Miss Dubourde` }
+	];
+
+
 </script>
+
+
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Kies je karakter</p>
+	<p>Pick a character:</p>
+    <select value={selected} on:change="{() => answer = ''}">
+		{#each characters as character}
+			<option value={character}>
+				{character.text}
+			</option>
+		{/each}
+	</select>
     <p>Naam</p>
     <p>Leeftijd</p>
     <p>Stat1</p>
@@ -12,6 +40,7 @@
     <p>Stat3</p>
     <p>Stat4</p>
 </main>
+
 
 <style>
 	main {
