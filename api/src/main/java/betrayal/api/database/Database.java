ccd1065f9f35162b2029eef030b98eb13083250f
EsraKlemann <98453430+EsraKlemann@users.noreply.github.com>
2022-04-05 14:10:14 +0200

    Queries for saving and loading characters added

--- api/src/main/java/betrayal/api/database/Database.java
+++ api/src/main/java/betrayal/api/database/Database.java
@@ -2,12 +2,15 @@ package betrayal.api.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 
 import betrayal.domain.Character;
+import betrayal.api.models.CharacterStatusDTO;
+import betrayal.api.models.CharacterSaveDTO;
 
 public class Database {
 
@@ -65,18 +68,86 @@ public class Database {
 
             c = new Character(name, age, speedStats, defaultSpIndex, mightStats,
                     defaultMiIndex, sanityStats, defaultSaIndex, knowledgeStats, defaultKnIndex);
-            System.out.println(resultSet.getString("name"));
-            System.out.println(resultSet.getString("Speedstats"));
+            // System.out.println(resultSet.getString("name"));
+            // System.out.println(resultSet.getString("Speedstats"));
 
         }
         return c;
     }
 
-    public void saveCharacterByPlayer() throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement
-                .executeQuery("INSERT into saved characters );
-                https://www.geeksforgeeks.org/how-to-insert-records-to-a-table-using-jdbc-connection/
+    public boolean saveCharacterByPlayer(CharacterSaveDTO csavedto) throws SQLException {
+
+        String name = csavedto.getName();
+        int age = csavedto.getAge();
+        int[] speedStats = csavedto.getSpeedStats();
+        String speedStatsStr = Arrays.toString(speedStats);
+        int defaultSpIndex = csavedto.getSpeedIndex()[0];
+        int[] mightStats = csavedto.getMightStats();
+        String mightStatsStr = Arrays.toString(mightStats);
+        int defaultMiIndex = csavedto.getMightIndex()[0];
+        int[] sanityStats = csavedto.getSanityStats();
+        String sanityStatsStr = Arrays.toString(sanityStats);
+        int defaultSaIndex = csavedto.getSanityIndex()[0];
+        int[] knowledgeStats = csavedto.getKnowledgeStats();
+        String knowledgeStatsStr = Arrays.toString(knowledgeStats);
+        int defaultKnIndex = csavedto.getKnowledgeIndex()[0];
+        int originalID = csavedto.getOriginalID();
+        String userName = csavedto.getUserName();
+
+        String query = "INSERT into `saved characters` (NameChar, Age, SpeedStats, DefaultSpeed, MightStats, DefaultMight, SanityStats, DefaultSanity, KnowledgeStats, DefaultKnowledge, originalID, namePlayer) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement preparedStmt = connection.prepareStatement(query);
+        preparedStmt.setString(1, name);
+        preparedStmt.setInt(2, age);
+        preparedStmt.setString(3, speedStatsStr);
+        preparedStmt.setInt(4, defaultSpIndex);
+        preparedStmt.setString(5, mightStatsStr);
+        preparedStmt.setInt(6, defaultMiIndex);
+        preparedStmt.setString(7, sanityStatsStr);
+        preparedStmt.setInt(8, defaultSaIndex);
+        preparedStmt.setString(9, knowledgeStatsStr);
+        preparedStmt.setInt(10, defaultKnIndex);
+        preparedStmt.setInt(11, originalID);
+        preparedStmt.setString(12, userName);
+
+        return preparedStmt.execute();
     }
 
+    public Character getCharacterByIdUsername(int id, String userName) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement
+                .executeQuery(
+                        "SELECT * FROM `saved characters` WHERE `originalID` = " + id + " AND `namePlayer` = \""
+                                + userName + "\"");
+
+        Character c = null;
+        while (resultSet.next()) {
+
+            String name = resultSet.getString("NameChar");
+            int age = resultSet.getInt("Age");
+            String speedStatsStr = resultSet.getString("Speedstats"); // string from DB split up to array of ints.
+            int[] speedStats = Arrays.stream(speedStatsStr.substring(1, speedStatsStr.length() - 1).split(", "))
+                    .mapToInt(s -> Integer.parseInt(s)).toArray();
+            int defaultSpIndex = resultSet.getInt("DefaultSpeed");
+            String mightStatsStr = resultSet.getString("MightStats");
+            int[] mightStats = Arrays.stream(mightStatsStr.substring(1, mightStatsStr.length() - 1).split(", "))
+                    .mapToInt(s -> Integer.parseInt(s)).toArray();
+            int defaultMiIndex = resultSet.getInt("DefaultMight");
+            String sanityStatsStr = resultSet.getString("SanityStats");
+            int[] sanityStats = Arrays.stream(sanityStatsStr.substring(1, sanityStatsStr.length() - 1).split(", "))
+                    .mapToInt(s -> Integer.parseInt(s)).toArray();
+            int defaultSaIndex = resultSet.getInt("DefaultSanity");
+            String knowledgeStatsStr = resultSet.getString("KnowledgeStats");
+            int[] knowledgeStats = Arrays
+                    .stream(knowledgeStatsStr.substring(1, knowledgeStatsStr.length() - 1).split(", "))
+                    .mapToInt(s -> Integer.parseInt(s)).toArray();
+            int defaultKnIndex = resultSet.getInt("DefaultKnowledge");
+
+            c = new Character(name, age, speedStats, defaultSpIndex, mightStats,
+                    defaultMiIndex, sanityStats, defaultSaIndex, knowledgeStats, defaultKnIndex);
+
+        }
+        return c;
+    }
 }
